afdb5040d86efee214bd2638697f3720fd9bb506
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2023-08-25 16:00:13 +0100

    fix

--- gameshelf/shelf/views.py
+++ gameshelf/shelf/views.py
@@ -1,14 +1,13 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth import get_user_model
 from django.http import HttpRequest
 
 from user_profile.models import ShelfUser
 
 @login_required
 def index(request: HttpRequest):
-    user: ShelfUser = get_user_model()
+    user: ShelfUser = request.user
 
-    print(user.games.get())
+    print(user.games.all())
 
     return render(request, "shelf/index.html")
