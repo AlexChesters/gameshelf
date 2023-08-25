3173d702e10845ee117b6ff7a1f66015d1888bef
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2023-08-25 15:34:34 +0100

    add many to many

--- gameshelf/user_profile/models.py
+++ gameshelf/user_profile/models.py
@@ -1,1 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+from shelf.models import Game
+
+class ShelfUser(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    games = models.ManyToManyField(Game)
