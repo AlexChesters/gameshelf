8108ecb2cf93728ab002c6a09ac00eeb777620f2
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2023-08-27 18:52:00 +0100

    dir

--- gameshelf/gameshelf/settings.py
+++ gameshelf/gameshelf/settings.py
@@ -82,11 +82,9 @@ WSGI_APPLICATION = "gameshelf.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db", "db.sqlite3"),
     }
 }
-
-print(f"DB path: {BASE_DIR} / db.sqlite")
 
 
 # Password validation
