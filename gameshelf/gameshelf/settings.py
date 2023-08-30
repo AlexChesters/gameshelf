a88e3d592721f6f2601999775ca34684f207f780
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2023-08-30 10:31:21 +0100

    remove comment

--- gameshelf/gameshelf/settings.py
+++ gameshelf/gameshelf/settings.py
@@ -18,10 +18,6 @@ from django.core.management.utils import get_random_secret_key
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
 SECRET_KEY = os.getenv("SECRET_KEY", get_random_secret_key())
 
 DEBUG = os.getenv("DEBUG_MODE", "").lower() == "true"
